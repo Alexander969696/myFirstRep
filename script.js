fa498e98379b6bc0777755ae7717be3d4535d966
Aleksander Afonin <alexanderafonin96@gmail.com>
2022-03-26 16:32:05 +0600

    completed the second lesson

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Studio Omsk';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 3000;
+let rollback = 55;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert("Hello World");
 
-console.log("My name is Sasha");
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+
+console.log(screens.length);
+console.log('Стоимость версти экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+
+let screensArray = screens.toLowerCase();
+screensArray = screensArray.split(', ');
+console.log(screensArray);
+
+let perRollback = fullPrice * (rollback / 100);
+console.log(perRollback); 
+
+
+
