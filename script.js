71b58270b0263945eb321172f82348cc7b4e8e57
Aleksander Afonin <alexanderafonin96@gmail.com>
2022-03-30 01:56:14 +0600

    Выполнил задание к 3 уроку

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-let title = 'Studio Omsk';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 3000;
-let rollback = 55;
-let fullPrice = 10000;
-let adaptive = true;
+'use strict'
+
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+let screenPrice = prompt('Сколько будет стоить данная работа?');
+let adaptive = confirm('Добавить ли адаптив на сайте?');
+let rollback = 32;
+
+let service1 = prompt('Какой дополнительной тип услуги нужен?');
+let servicePrice1 = prompt('Цена данной услуги?');
+let service2 = prompt('Какой дополнительной тип услуги нужен?');
+let servicePrice2 = prompt('Цена данной услуги?');
+let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
+let servicePercentPrice = fullPrice - (fullPrice*(rollback/100));
+
+console.log(Math.ceil(servicePercentPrice));
+
+if (fullPrice > 29999) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice > 14999 && fullPrice < 30000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice == 0) {
+    console.log('Стоимость должна быть больше 0');
+} else if (fullPrice < 0) {
+    console.log('Что-то пошло не так');
+}
 
 
-console.log(typeof(title));
-console.log(typeof(fullPrice));
-console.log(typeof(adaptive));
 
-console.log(screens.length);
-console.log('Стоимость версти экранов ' + screenPrice + ' рублей');
-console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
 
-let screensArray = screens.toLowerCase().split(', ');
-console.log(screensArray);
 
-let perRollback = fullPrice * (rollback / 100);
-console.log(perRollback); 
+
+
+
 
 
 
